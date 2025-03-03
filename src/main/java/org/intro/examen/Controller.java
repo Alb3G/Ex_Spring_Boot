4239fa1870a083577848e024d13462907a742bcb
Alberto <guzmanmorenoalberto1998@gmail.com>
2025-03-03 08:03:14 +0100

    Preparando examen

---
+++ src/main/java/org/intro/examen/Controller.java
@@ -0,0 +1,12 @@
+package org.intro.examen;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/examen")
+public class Controller {
+
+
+
+}
